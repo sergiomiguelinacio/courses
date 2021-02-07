60d093df98b8d253f11243c6359aab07f73576cc
Sergio Silva <sergio.miguel.inacio>
2021-02-07 22:16:18 +0000

    variables datatypes of numbers

---
+++ Kotlin/HelloWorld/app/src/main/java/com/example/helloworld/KotlinBasics.kt
@@ -0,0 +1,23 @@
+package com.example.helloworld
+
+fun main() {
+    // immutable variable
+    // type string
+    val myName = "Sergio"
+
+    // type int 32 bit
+    var myAge = 31
+
+    // Integer TYPES: Byte(8 bit), Short (16 bit),
+    // Int (32 bit), Long (64bit)
+    val myByte : Byte = 13
+    val myShort : Short = 125
+    val myInt : Int = 123123123
+    val myLong : Long = 39_812_309_487_120_300
+
+    // Floating Point number Types: Float (32 bit), Double (64 bit)
+    val myFloat: Float = 13.37F
+    val myDouble: Double = 3.141559265358979323846
+
+    print("Hello " + myName)
+}
