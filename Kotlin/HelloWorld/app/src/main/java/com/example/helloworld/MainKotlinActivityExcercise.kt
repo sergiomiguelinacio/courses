de134d8c8e4ba4c95898b70ee09c973fab9e506f
Sergio Silva <sergio.miguel.inacio>
2021-02-08 20:12:40 +0000

    Exercise

---
+++ Kotlin/HelloWorld/app/src/main/java/com/example/helloworld/MainKotlinActivityExcercise.kt
@@ -0,0 +1,12 @@
+package com.example.helloworld
+
+fun main(){
+    var stringVar : String = "Android Masterclas"
+    var floatVar : Float = 13.37F
+    var doubleVar : Double = 3.14159265358979
+    var shortVar: Short = 25
+    var intVar: Int = 2020
+    var longVar: Long = 18881234567
+    var boolVar : Boolean = true
+    var charVar : Char = 'a'
+}
