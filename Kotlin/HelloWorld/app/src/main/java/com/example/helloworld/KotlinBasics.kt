5845e6bdc8d2781119469573f7c9ed3067e8756d
Sergio Silva <sergio.miguel.inacio>
2021-02-08 21:41:31 +0000

    When Expressions - Including Exercises

--- Kotlin/HelloWorld/app/src/main/java/com/example/helloworld/KotlinBasics.kt
+++ Kotlin/HelloWorld/app/src/main/java/com/example/helloworld/KotlinBasics.kt
@@ -111,4 +111,40 @@ fun main() {
         println("It's rainy")
 
 
+    // when expressions
+    var season = 3
+    when(season){
+        1 -> println("Spring")
+        2 -> println("Summer")
+        3 -> {
+            println("Fall")
+            println("Autumn")
+        }
+        4 -> println("Winter")
+        else -> println("Invalid Season")
+    }
+
+    var month = 3
+    when(month){
+        in 3..5 -> println("Spring")
+        in 6..8 -> println("Summer")
+        in 9..11 -> println("Fall")
+        12, 1, 2 -> println("Winter")
+        else -> println("Invalid Season")
+    }
+
+    when(age){
+        !in 0..20 -> println("now you may drink in th US")
+        in 18..20 -> println("you may vote now")
+        in 16..17 -> println("you're too young")
+    }
+
+    var x : Any = "13.37f"
+
+    when(x){
+        is Int -> println("$x is an Int")
+        !is Double -> println("$x is not a Double")
+        is String -> println("$x is String")
+        else -> println("$x is none of the above")
+    }
 }
