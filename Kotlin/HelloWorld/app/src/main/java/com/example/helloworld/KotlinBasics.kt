10aacc29ec9f0ed64fd5249f2b69beffbd134a24
Sergio Silva <sergio.miguel.inacio>
2021-02-08 22:14:59 +0000

    For Loops

--- Kotlin/HelloWorld/app/src/main/java/com/example/helloworld/KotlinBasics.kt
+++ Kotlin/HelloWorld/app/src/main/java/com/example/helloworld/KotlinBasics.kt
@@ -179,4 +179,18 @@ fun main() {
             println("it's comfy now")
         }
     }
+
+    for(num in 1..10){
+        print("$num")
+    }
+
+    for(i in 1 until 10){
+        print("$i ")
+    }
+
+    println("_________")
+
+    for(i in 10 downTo  1 step 2){ // Same as - for(i im 10.downTo(1).step(2))
+        print("$i ")
+    }
 }
