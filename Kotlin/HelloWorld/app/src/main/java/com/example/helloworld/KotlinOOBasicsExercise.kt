963df8562a630d1cc9d32dfd61d87864603cac3a
Sergio Silva <sergio.miguel.inacio>
2021-02-09 23:38:53 +0000

    Exercise Classes and Objects

---
+++ Kotlin/HelloWorld/app/src/main/java/com/example/helloworld/KotlinOOBasicsExercise.kt
@@ -0,0 +1,14 @@
+package com.example.helloworld
+
+fun main(){
+    var samsung = MobilePhone("Android", "Samsung", "Galaxy S20 Ultra")
+    var iphone = MobilePhone("IOS", "Apple", "7")
+}
+
+class MobilePhone(osName: String, brand: String, model: String){
+
+    init {
+        println("Initializer a new MobilePhone object with " +
+                "osName = $osName,  brand = $brand and model = $brand")
+    }
+}
