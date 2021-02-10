8027ad971ee05094385c4c43c7557cbc15ef1c8e
Sergio Silva <sergio.miguel.inacio>
2021-02-10 00:08:05 +0000

    Member Variables - Functions And Constructors

--- Kotlin/HelloWorld/app/src/main/java/com/example/helloworld/KotlinOOBasics.kt
+++ Kotlin/HelloWorld/app/src/main/java/com/example/helloworld/KotlinOOBasics.kt
@@ -1,9 +1,15 @@
 package com.example.helloworld
 
 fun main() {
-    var sergio = Person("Sergio", "Silva")
+    var sergio = Person("Sergio", "Silva", 31)
+    sergio.hobby = "skateboard"
+    sergio.age = 34
+    println("Sergio is ${sergio.age} years old")
+    sergio.stateHooby()
     var john = Person()
-    var johnPeterson = Person(lastName = "Peterson")
+    john.hobby = "play video games"
+    john.stateHooby()
+//    var johnPeterson = Person(lastName = "Peterson")
 
 
     myFunction(5)
@@ -11,10 +17,31 @@ fun main() {
 
 class Person(firstName: String = "John", lastName: String = "Doe"){
 
+    // Member Variable - Properties
+    var age : Int? = null
+    var hobby: String = "Watch Netflix"
+    var personFirstName : String? = null
+
     // Initializer Block
     init {
+        this.personFirstName = firstName
         println("Initializer a new Person object with " +
                 "firstName = $firstName and lastName = $lastName")
+    }
+
+    // Member secondary Constructor
+    constructor(firstName: String, lastName: String, age: Int)
+        : this(firstName, lastName){
+        this.age = age
+        println("Initializer a new Person object with " +
+                "firstName = $firstName and lastName = $lastName and age = $age")
+        }
+
+
+
+    // Member function - Methods
+    fun stateHooby(){
+        println("$personFirstName\'s hobby is $hobby")
     }
 }
 
