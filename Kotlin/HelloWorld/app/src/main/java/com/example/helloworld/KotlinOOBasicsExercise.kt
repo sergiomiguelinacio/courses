6d872e9557aca022592af30c3f75966c625f5a13
Sergio Silva <sergio.miguel.inacio>
2021-02-14 20:48:03 +0000

    Challange classes

--- Kotlin/HelloWorld/app/src/main/java/com/example/helloworld/KotlinOOBasicsExercise.kt
+++ Kotlin/HelloWorld/app/src/main/java/com/example/helloworld/KotlinOOBasicsExercise.kt
@@ -3,13 +3,21 @@ package com.example.helloworld
 fun main(){
     var samsung = MobilePhone("Android", "Samsung", "Galaxy S20 Ultra")
     var iphone = MobilePhone("IOS", "Apple", "7")
+    iphone.chageBattery(50)
 }
 
 class MobilePhone(osName: String, brand: String, model: String){
 
+    private var battery = 0
+
     init {
         println("Initializer a new MobilePhone object with " +
                 "osName = $osName,  brand = $brand and model = $brand")
+    }
+
+    fun chageBattery(charge : Int){
+        println("Battery before chage = $battery with charge the battery status is ${battery + charge}")
+        battery += charge
     }
 }
 
