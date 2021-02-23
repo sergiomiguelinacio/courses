da91e68658e4f4926aeed21d7e2434662d1a4c5b
Sergio Silva <sergio.miguel.inacio>
2021-02-23 02:47:12 +0000

    My calculator add functionality

--- Kotlin/MyCalculator/app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ Kotlin/MyCalculator/app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -3,15 +3,66 @@ package com.example.mycalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import com.example.mycalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    var lastNumeric : Boolean = false
+    var lastDot : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     fun onDigit(view: View){
-        Toast.makeText(this, "Button works", Toast.LENGTH_SHORT).show()
+        var tvInput = binding.tvInput as TextView
+
+        tvInput.append((view as Button).text)
+        lastNumeric = true
+    }
+
+    fun onClear(view : View){
+        var tvInput = binding.tvInput as TextView
+
+        tvInput.text = ""
+        lastNumeric = false
+        lastDot = false
+    }
+
+    fun onDecimalPoint(view : View){
+        var tvInput = binding.tvInput as TextView
+
+        if(lastNumeric && !lastDot){
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+
+    fun onOperator(view: View){
+        var tvInput = binding.tvInput as TextView
+
+        if(lastNumeric && !isOperatorAdded(tvInput.text.toString())){
+            tvInput.append((view as Button).text)
+            lastNumeric = false
+            lastDot = false
+        }
+    }
+
+    private fun isOperatorAdded(value : String) : Boolean{
+        return if (value.startsWith("-")){
+            false
+        }
+        else {
+            value.contains("/") || value.contains("*")
+                    || value.contains("+") || value.contains("-")
+        }
     }
 }
